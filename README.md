724573bbb6be503d8531b357d4f306b57c554af2
Sankar Siva <siva0sankar@gmail.com>
2018-09-29 00:44:55 +0530

    DAY 2 - Video 2 - Where I'm craeted DataBase model User.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Mean-Mail
 Mail Client Using MEAN Strack
+
+This Project is just an start up . As of now I have tried to learn MEAN stack . Once I have learned all of it. Will start to work o creating email client for other mails like Gmail, Microsoft mail, etc.
+
+In this Project I have used more number of node modules . I will give short note for it in this file
+
+#express
+I have used this express module as web application fremework
+
+#morgan
+I have used morgan for logging perpose. It will save the log of application. As of now i have used "dev" mode for logging.
+
+#mongoose
+mongoose is used to connect MongoDB. this is module is necessary to connect MongoDB.
+
+#body-parser
+body-parser used to parsing JSON format while giving data from POST method.
+
+#bcrypt-nodejs
+bcrypt-nodejs is a middleware of mongooes module. This was used for encrypting password value .
+
+#Advanced REST Client - ARC
+This is Google Chrome Extension . I have used this extension for craeting post  request. we can used other tools also for this prepose like POSTMAN.
--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var User = require('./app/models/user');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(morgan('dev'));
 
 mongoose.connect('mongodb://localhost:27017/mail',function(err){
     if(err){
@@ -12,8 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mail',function(err){
     }
 });
 
-app.use(morgan('dev'));
-
 app.get('/',function(req,res){
     res.send('Hellow World..');
 })
@@ -21,6 +26,31 @@ app.get('/',function(req,res){
 app.get('/home',function(req,res){
     res.send('Hellow Home ..:)')
 });
+
+//http://localhost:8080/user
+app.post('/user',function(req,res){
+    //  res.send('Testing User..');  -- Just for Testing 
+
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    if(req.body.username == null ||req.body.username == '' ||
+        req.body.password == null ||req.body.password == '' ||
+        req.body.email == null ||req.body.email == '' ){
+        res.send("Ensure username,email and password were provided");
+    }else{
+        user.save(function(err){
+            if(err){
+                res.send('Username or Email is already created.. :(');
+            }else{
+                res.send('user created :)');
+            }
+        });
+    }
+
+
+})
 
 app.listen(port,function(){
     console.log('Server Started... on port = '+ port);
